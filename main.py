ce96c92b987be912d98312715858116325eab7fa
rubenbuelvas <rcbuelvasv@gmail.com>
2021-06-20 16:25:42 -0400

    svr declaration

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import requests
 import os.path
 import pandas as pd
 from sklearn.svm import SVR
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+
 
 # Constants
 DATASET_NAME = 'ICFES_dataset.csv'
@@ -38,7 +42,15 @@ def download_if_not_exists(filename, url):
 
 if __name__ == '__main__':
     download_if_not_exists(DATASET_NAME, 'https://www.datos.gov.co/api/views/ynam-yc42/rows.csv?accessType=DOWNLOAD')
-    data = pd.read_csv(DATASET_NAME)
-    y = data['PUNT_GLOBAL']
-    data.
+    df = pd.read_csv(DATASET_NAME)
+    y = df['PUNT_GLOBAL']
+    for column in df.columns:
+        if 'PUNT' in column or 'DESEMP' in column or 'PERCENTIL' in column:
+            df = df.drop(column, axis=1)
+    X = df
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=0)
+    svr = SVR()
+    svr.fit(X_train, y_train)
+    # TODO One hot encoding
+    y_pred = svr.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
